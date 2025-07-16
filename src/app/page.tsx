49dd3d2c7380fa7e27b4ad0fc7e95b933eb49c74
Tommaso Balzarini <tommasobalzarini02@gmail.com>
2025-07-16 09:25:46 +0000

    aggiungi la funzione di poter visualizzare la versione scura o chiare de

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Dashboard } from "@/components/dashboard";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   return (
     <div className="flex-1 space-y-4 p-4 sm:p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
-        <h1 className="text-3xl font-bold tracking-tight text-gray-800 dark:text-white">
+        <h1 className="text-3xl font-bold tracking-tight">
           Riepilogo Attività
         </h1>
+        <ThemeToggle />
       </div>
       <Dashboard />
     </div>
